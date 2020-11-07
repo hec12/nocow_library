1993006627bc0f1d1e9d4678a00685ca6bd5e611
hec <hecaton.prco@gmail.com>
2020-11-08 04:05:06 +0900

    Fix a bug of KMP

--- src/string/KMP.cpp
+++ src/string/KMP.cpp
@@ -4,22 +4,25 @@
 
 // kmp[i]=s[0,i-1]のprefixとsuffixの最長共通文字列の長さ
 auto init(const string &s) {
-	int n = s.size(), j = -1;
-	vector<int> kmp(n + 1, -1);
+    int n = s.size(), j = -1;
+    vector<int> kmp(n + 1, -1);
 
-	rep(i, n) {
-		while (j >= 0 && s[i] != s[j]) j = kmp[j];
-		j++, kmp[i + 1] = (s[i] == s[j]) ? kmp[j] : j;
-	}
-	return kmp;
+    rep(i, n) {
+        while (j >= 0 && s[i] != s[j])
+            j = kmp[j];
+        j++, kmp[i + 1] = (i + 1 < n and s[i + 1] == s[j]) ? kmp[j] : j;
+    }
+    return kmp;
 }
 
 int match(string p, string s, const auto &kmp) {
-	int cur = 0, m = p.size(), n = s.size();
-	rep(i, n) {
-		while (cur >= 0 && s[i] != p[cur]) cur = kmp[cur];
-		cur++;
-		if (cur >= m) return cur; // cur=kmp[cur];
-	}
-	return cur;
+    int cur = 0, m = p.size(), n = s.size();
+    rep(i, n) {
+        while (cur >= 0 && s[i] != p[cur])
+            cur = kmp[cur];
+        cur++;
+        if (cur >= m)
+            return cur;  // cur=kmp[cur];
+    }
+    return cur;
 }
